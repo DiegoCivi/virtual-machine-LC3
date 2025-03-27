a3559f0d7d0ecbce00607acd890811e021a11f6d
Diego Civini <diego.civini@lambdaclass.com>
2025-03-27 18:04:13 -0300

    Add documentation to the utils functions

--- src/utils.rs
+++ src/utils.rs
@@ -1,5 +1,8 @@
 use crate::hardware::{CondFlag, Register, Registers};
 
+/// Extends a number represented in 'bit_count' bits into
+/// 16 bits, always taking into account the sign of
+/// the original number
 pub fn sign_extend(mut x: u16, bit_count: usize) -> u16 {
     // Get MSB and check if it is a 1
     let msb = x >> (bit_count - 1) & 1; // TODO: Is the '& 1' necessary?
@@ -10,6 +13,7 @@ pub fn sign_extend(mut x: u16, bit_count: usize) -> u16 {
     x
 }
 
+/// It updates the register COND where we have the condition flag
 pub fn update_flags(r: Register, regs: &mut Registers) {
     if regs[r] == 0 {
         regs[Register::COND] = CondFlag::ZRO as u16;
