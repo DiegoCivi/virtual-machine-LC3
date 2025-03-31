f6341a1b4ea0e6b1d84cb89c620af7e89700db27
Diego Civini <diego.civini@lambdaclass.com>
2025-03-31 18:43:43 -0300

    Add tests for all trap routines

--- src/trap_routines.rs
+++ src/trap_routines.rs
@@ -67,10 +67,146 @@ fn puts_p(regs: &mut Registers, mem: &mut Memory, writer: &mut impl Write) -> Re
     Ok(())
 }
 
-fn halt(mut running: bool, writer: &mut impl Write) -> Result<(), VMError> {
+fn halt(running: &mut bool, writer: &mut impl Write) -> Result<(), VMError> {
     let s = "HALT\n".as_bytes();
     stdout_write(s, writer)?;
     stdout_flush(writer)?;
-    running = false;
+    *running = false;
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use std::io::Cursor;
+
+    use crate::hardware::{Register, Registers};
+
+    use super::*;
+
+    #[test]
+    fn get_c_modifies_R0_with_input_char() {
+        let char = "c";
+        let char_bytes: u16 = char.as_bytes()[0].into();
+        let mut reader = Cursor::new(char);
+        let mut regs = Registers::new();
+        let _ = get_c(&mut regs, &mut reader);
+
+        assert_eq!(regs[Register::R0], char_bytes);
+    }
+
+    #[test]
+    fn trap_out_writes_R0_value() {
+        let char = "c";
+        let char_bytes: u16 = char.as_bytes()[0].into();
+        let mut writer: Vec<u8> = Vec::new();
+        let mut regs = Registers::new();
+        regs[Register::R0] = char_bytes;
+        let _ = trap_out(&mut regs, &mut writer);
+
+        let written_val: u16 = writer[0].into();
+        assert_eq!(written_val, char_bytes);
+    }
+
+    #[test]
+    fn trap_in_writes_R0_with_reader_value() {
+        let char = "c";
+        let char_bytes: u16 = char.as_bytes()[0].into();
+        let mut reader = Cursor::new(char);
+        let mut writer: Vec<u8> = Vec::new();
+        let mut regs = Registers::new();
+        let _ = trap_in(&mut regs, &mut writer, &mut reader);
+
+        assert_eq!(regs[Register::R0], char_bytes);
+    }
+
+    #[test]
+    fn trap_in_writes_writer_with__value() {
+        let char = "c";
+        let char_bytes: u16 = char.as_bytes()[0].into();
+        let mut reader = Cursor::new(char);
+        let mut writer: Vec<u8> = Vec::new();
+        let mut regs = Registers::new();
+        let _ = trap_in(&mut regs, &mut writer, &mut reader);
+
+        let written_val: u16 = writer[0].into();
+        assert_eq!(written_val, char_bytes);
+    }
+
+    #[test]
+    fn puts_writes_whole_string_on_writer() {
+        let mut writer: Vec<u8> = Vec::new();
+        let (char1, char2, char3) = ("1", "2", "3");
+        let char1_bytes: u16 = char1.as_bytes()[0].into();
+        let char2_bytes: u16 = char2.as_bytes()[0].into();
+        let char3_bytes: u16 = char3.as_bytes()[0].into();
+        let starting_address: u16 = 0x0005;
+
+        let mut regs = Registers::new();
+        regs[Register::R0] = starting_address;
+        let mut mem = Memory::new();
+        let _ = mem.write(starting_address, char1_bytes);
+        let _ = mem.write(starting_address + 1, char2_bytes);
+        let _ = mem.write(starting_address + 2, char3_bytes);
+
+        let _ = puts(&mut regs, &mut mem, &mut writer);
+
+        let written_val_1: u16 = writer[0].into();
+        let written_val_2: u16 = writer[1].into();
+        let written_val_3: u16 = writer[2].into();
+        assert_eq!(written_val_1, char1_bytes);
+        assert_eq!(written_val_2, char2_bytes);
+        assert_eq!(written_val_3, char3_bytes);
+    }
+
+    #[test]
+    fn halt_changes_bool() {
+        let mut writer: Vec<u8> = Vec::new();
+        let mut running = true;
+
+        let _ = halt(&mut running, &mut writer);
+
+        assert!(!running);
+    }
+
+    #[test]
+    fn halt_writes_on_writer() {
+        let mut writer: Vec<u8> = Vec::new();
+        let mut running = true;
+        let expected_bytes = "HALT\n".as_bytes();
+        let _ = halt(&mut running, &mut writer);
+
+        assert_eq!(writer, expected_bytes);
+    }
+
+    #[test]
+    fn puts_p_run_writes_on_writer() {
+        let mut writer: Vec<u8> = Vec::new();
+        let (char1, char2, char3, char4) = ("1", "2", "3", "4");
+        let char1_bytes: u16 = char1.as_bytes()[0].into();
+        let char2_bytes: u16 = char2.as_bytes()[0].into();
+        let char3_bytes: u16 = char3.as_bytes()[0].into();
+        let char4_bytes: u16 = char4.as_bytes()[0].into();
+
+        let memory_location1: u16 = (char2_bytes << 8) | char1_bytes;
+        let memory_location2: u16 = (char4_bytes << 8) | char3_bytes;
+
+        let starting_address: u16 = 0x0005;
+        let mut regs = Registers::new();
+        regs[Register::R0] = starting_address;
+        let mut mem = Memory::new();
+        let _ = mem.write(starting_address, memory_location1);
+        let _ = mem.write(starting_address + 1, memory_location2);
+    
+        let _ = puts_p(&mut regs, &mut mem, &mut writer);
+
+
+        let written_val_1: u16 = writer[0].into();
+        let written_val_2: u16 = writer[1].into();
+        let written_val_3: u16 = writer[2].into();
+        let written_val_4: u16 = writer[3].into();
+        assert_eq!(written_val_1, char1_bytes);
+        assert_eq!(written_val_2, char2_bytes);
+        assert_eq!(written_val_3, char3_bytes);
+        assert_eq!(written_val_4, char4_bytes);
+    }
 }
