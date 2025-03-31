788129da38a78aae185ab1a1ba24b5614c2c1ef3
Diego Civini <diego.civini@lambdaclass.com>
2025-03-31 14:52:02 -0300

    Fix error from merge

--- src/utils.rs
+++ src/utils.rs
@@ -7,8 +7,8 @@ use crate::{error::VMError, hardware::{CondFlag, Register, Registers}};
 /// the original number
 pub fn sign_extend(mut x: u16, bit_count: usize) -> Result<u16, VMError> {
     // Get MSB and check if it is a 1
-    let bitcount_sub = bit_count.checked_sub(1).ok_or(VMError::ArithmeticError)?;
-    let msb = (x >> bitcount_sub);
+    let bitcount_sub = bit_count.checked_sub(1).ok_or(VMError::Arithmetic)?;
+    let msb = x >> bitcount_sub;
     if msb != 0 {
         // If the MSB is 1 it means it is negative, else its positive
         x |= 0xFFFF << bit_count;
