5ec2683474e27e2afbfe99ea20c6fddbec743dad
Diego Civini <diego.civini@lambdaclass.com>
2025-04-03 13:15:55 -0300

    Add match with instructions on main

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 use std::env;
 
 use error::VMError;
-use hardware::{Memory, Register, Registers};
+use hardware::{CondFlag, Memory, OpCode, Register, Registers};
 use utils::{load_arguments, setup, shutdown};
+use instructions::*;
 
 mod error;
 mod hardware;
@@ -21,10 +22,34 @@ fn main() -> Result<(), VMError> {
     load_arguments(&mut args, &mut mem)?;
     // Setup
     let termios = setup()?;
+    regs[Register::Cond] = CondFlag::Zro.value();
     regs[Register::PC] = PC_START;
 
     // VM main loop
-    /* TODO! */
+    let mut running = true;
+    while running {
+        let instr_addr = regs[Register::PC];
+        regs[Register::PC] = regs[Register::PC].wrapping_add(1);
+        let instr = mem.read(instr_addr)?;
+        let op_code = OpCode::try_from(instr >> 12)?;
+        match op_code {
+            OpCode::Br => branch(instr, &mut regs).unwrap(),
+            OpCode::Add => add(instr, &mut regs).unwrap(),
+            OpCode::Ld => load(instr, &mut regs, &mut mem).unwrap(),
+            OpCode::St => store(instr, &mut regs, &mut mem).unwrap(),
+            OpCode::Jsr => jump_register(instr, &mut regs).unwrap(),
+            OpCode::And => and(instr, &mut regs).unwrap(),
+            OpCode::Ldr => load_register(instr, &mut regs, &mut mem).unwrap(),
+            OpCode::Str => store_register(instr, &mut regs, &mut mem).unwrap(),
+            OpCode::Not => not(instr, &mut regs).unwrap(),
+            OpCode::Ldi => load_indirect(instr, &mut regs, &mut mem).unwrap(),
+            OpCode::Sti => store_indirect(instr, &mut regs, &mut mem).unwrap(),
+            OpCode::Jmp => jump(instr, &mut regs).unwrap(),
+            OpCode::Lea => load_effective_address(instr, &mut regs).unwrap(),
+            OpCode::Trap => trap(instr, &mut regs, &mut mem, &mut running_flag).unwrap(),
+        }
+
+    }
 
     // Shutdown
     shutdown(termios)?;
